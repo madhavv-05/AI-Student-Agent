727e9b02724ab60e94b389611b9d2befd5e0d072
madhavguptaa <madhav87308@gmail.com>
2025-07-04 01:05:48 +0530

    updated reduce_noise()

--- audio_processing.py
+++ audio_processing.py
@@ -12,6 +12,7 @@ def bandpass_filter(audio_data, rate, lowcut=300, highcut=3400):
     return filtered_audio.astype(np.int16).tobytes()
 
 def reduce_noise(audio_data, rate):
+    
     audio_array = np.frombuffer(audio_data, dtype=np.int16)
     noise_profile = audio_array[:rate] if len(audio_array) > rate else audio_array
     reduced_audio = nr.reduce_noise(y=audio_array, sr=rate, y_noise=noise_profile)
